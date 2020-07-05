27b6eec9956d18137a544b78d1f01800722790b3
1Baldr <47713981+1Baldr@users.noreply.github.com>
2020-07-05 23:19:38 +0300

    Updates

---
+++ precache-manifest.0cf8672aafe52db54825629e623ca93a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025b14ffc4210ca83ca78cc7ce7f0025",
+    "url": "/Task_dev/index.html"
+  },
+  {
+    "revision": "df74acf3aa80b9210ea4",
+    "url": "/Task_dev/static/css/main.3b826f3c.chunk.css"
+  },
+  {
+    "revision": "e26f3b49cda25c8c1e51",
+    "url": "/Task_dev/static/js/2.36960f10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Task_dev/static/js/2.36960f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df74acf3aa80b9210ea4",
+    "url": "/Task_dev/static/js/main.9cb3aa93.chunk.js"
+  },
+  {
+    "revision": "577b0e268386c1fc410e",
+    "url": "/Task_dev/static/js/runtime-main.bc6ef1de.js"
+  }
+]);
